d2341543d207e65a4b478d53cacf2fc323ade00e
mubler <giardina.alessandro.iphone@gmail.com>
2017-11-08 17:10:11 +0100

    ultimo commit

--- esempioSqlInjection/src/it/tecnetdati/esempio/Main.java
+++ esempioSqlInjection/src/it/tecnetdati/esempio/Main.java
@@ -27,8 +27,7 @@ public class Main {
 		//String cognome="Giardina";
 		String cognome ="Alessio ora ci crede!!!!' OR '1=1";
 
-		//io invece qui
-		ciao
+		
 		
 		StudenteDaoITF studenteDao=null;
 		try 
